51079d71c669747b714e6aabe2075f102b61bab4
João Soares <145237422+joaosoares30@users.noreply.github.com>
2026-09-03 11:47:09 -0300

    Add AlgoritmoDoisAl class with main method

---
+++ AlgoritmoDoisAl.java
@@ -0,0 +1,14 @@
+
+public class AlgoritmoDoisAl {
+    void main(){
+        IO.println("Quem ousa entrar na sala do Eco?");
+        String nome = IO.readln();
+        IO.println("Ah... " + nome + " Eu esperava por você.");
+        
+        IO.println("Quantos algoritmos Você já venceu?");
+        String resposta = IO.readln();
+        int total = Integer.parseInt(resposta);
+    IO.println("Só mais " + (360 - total) + " pela frente.");
+    }
+    
+}
