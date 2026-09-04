80dc87b175180aadece604b6eb39bc11ff8db89e
João Soares <145237422+joaosoares30@users.noreply.github.com>
2026-09-04 09:26:40 -0300

    Add files via upload

---
+++ AlgoritmoCincoAl.java
@@ -0,0 +1,8 @@
+public class AlgoritmoCincoAl {
+    void main() {
+        int num1 = Integer.parseInt(IO.readln("Informe um número: "));
+        IO.println("Número Anterior: " + (num1 - 1));
+        IO.println("Número : " + num1);
+        IO.println("Número Posterior: " + (num1 + 1));
+    }
+}
