1d915be45f5f3202d5b405d71daf8a2b8dd9d490
João Soares <145237422+joaosoares30@users.noreply.github.com>
2026-09-04 09:50:58 -0300

    Add files via upload

---
+++ AlgortimoSeteAl.java
@@ -0,0 +1,9 @@
+public class AlgortimoSeteAl {
+    void main() {
+        int num1 = Integer.parseInt(IO.readln("Informe um Número: "));
+        int num2 = Integer.parseInt(IO.readln("Informe outro Número: "));
+        
+        System.out.println("Soma: " + (num1 + num2));
+    }
+    
+}
