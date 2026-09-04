5c7d4e42775e3e9f2d82f0b3d1ceca8b7ed7d22b
João Soares <145237422+joaosoares30@users.noreply.github.com>
2026-09-04 10:02:11 -0300

    Add files via upload

---
+++ AlgorittmoOitoAl.java
@@ -0,0 +1,10 @@
+public class AlgorittmoOitoAl {
+    void main() {
+        int num1 = Integer.parseInt(IO.readln("Informe o Primeiro Número: "));
+        int num2 = Integer.parseInt(IO.readln("Informe outro Número: "));
+
+        System.out.println("Produto: " + (num1 * num2));
+
+
+    }
+}
