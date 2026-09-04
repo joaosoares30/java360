66e513d564d91f6adbf3b12f02075f8558bc67f0
João Soares <145237422+joaosoares30@users.noreply.github.com>
2026-09-04 10:00:14 -0300

    Add files via upload

---
+++ AlgoritmoDezAl.java
@@ -0,0 +1,8 @@
+public class AlgoritmoDezAl {
+    void main() {
+        int num1 = Integer.parseInt(IO.readln("Informe o Primeiro Número: "));
+        int num2 = Integer.parseInt(IO.readln("Informe outro Número: "));
+
+        System.out.println("Media: " + (num1 + num2)/ 2);
+    }
+}
