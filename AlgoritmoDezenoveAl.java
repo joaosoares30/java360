cde0e9b6810a686b4c2fcbd62ee7801dc7f95441
João Soares <145237422+joaosoares30@users.noreply.github.com>
2026-09-09 21:28:36 -0300

    Add files via upload

---
+++ AlgoritmoDezenoveAl.java
@@ -0,0 +1,8 @@
+import java.util.List;
+public class AlgoritmoDezenoveAl{
+    public void main(){
+        List<String> user = List.of("João", "luciana", "Meirelles", "Ana");
+
+        user.forEach(n -> System.out.println(n));
+    }
+}
---
+++ AlgoritmoVinteAl.java
@@ -0,0 +1,10 @@
+import java.util.List;
+public class AlgoritmoVinteAl {
+    public static void main(String[] args) {
+        
+        List<String> promo = List.of("PROMO1", "PROMO2", "DESCONTO1");
+
+        promo.forEach(p -> System.out.println(p));
+
+    }
+}
